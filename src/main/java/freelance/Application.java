5660d4da929c56a3f623573f7afd08d9d287d953
Theophane Kouchoanou <ptitkouche@yahoo.fr>
2022-12-10 22:19:14 +0100

    freelance project start

---
+++ src/main/java/freelance/Application.java
@@ -0,0 +1,9 @@
+package freelance;
+
+import com.sun.tools.javac.Main;
+
+public class Application {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
