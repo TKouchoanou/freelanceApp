9902318372e7b8d182374aaad0b46f8455879f1a
Theophane Kouchoanou <ptitkouche@yahoo.fr>
2022-12-11 12:19:34 +0100

    added spring boot dependency with configuration of logger

--- src/main/java/freelance/Application.java
+++ src/main/java/freelance/Application.java
@@ -1,9 +1,19 @@
 package freelance;
 
-import com.sun.tools.javac.Main;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InjectionPoint;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 
+@SpringBootApplication
 public class Application {
     public static void main(String[] args){
         System.out.println("hello world");
+    }
+    @Bean @Scope("prototype")
+    Logger logger(InjectionPoint ip){
+        return LoggerFactory.getLogger(ip.getMember().getDeclaringClass());
     }
 }
