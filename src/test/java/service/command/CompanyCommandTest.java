2d0ad5a3aa3c80936e13729713bfce9520961fb4
Theophane Kouchoanou <ptitkouche@yahoo.fr>
2023-03-20 22:03:17 +0100

    add update company command handler test

--- src/test/java/service/command/CompanyCommandTest.java
+++ src/test/java/service/command/CompanyCommandTest.java
@@ -2,6 +2,7 @@ package service.command;
 
 import freelance.service.command.CommandException;
 import freelance.service.command.command.company.CreateCompanyCommand;
+import freelance.service.command.command.company.UpdateCompanyCommand;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +19,25 @@ public class CompanyCommandTest {
         Assertions.assertThrows(CommandException.class, command::validateStateAfterHandling);
         command.setCompanyId(1L);
         Assertions.assertDoesNotThrow(command::validateStateAfterHandling);
+    }
+   @Test
+    public void  updateValidCompany(){
+        CreateCompanyCommand command= CreateCompanyCommand
+                .builder()
+                .ribId(1L)
+                .name("TK Buisness")
+                .build();
+        String currentName="TKS Buisness";
+
+       UpdateCompanyCommand updateCompanyCommand= UpdateCompanyCommand.builder().build();
+       Assertions.assertThrows(CommandException.class,updateCompanyCommand::validateStateBeforeHandling);
+       updateCompanyCommand.setCompanyId(command.getCompanyId());
+       Assertions.assertThrows(CommandException.class,updateCompanyCommand::validateStateBeforeHandling);
+       updateCompanyCommand.setName(currentName);
+       Assertions.assertThrows(CommandException.class,updateCompanyCommand::validateStateBeforeHandling);
+       updateCompanyCommand.setRibId(command.getRibId());
+       Assertions.assertDoesNotThrow(command::validateStateBeforeHandling);
+
+        Assertions.assertDoesNotThrow(updateCompanyCommand::validateStateAfterHandling);
     }
 }
