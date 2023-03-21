68064a2f9a0665bae44973bfb33d5e4351596ddc
Theophane Kouchoanou <ptitkouche@yahoo.fr>
2023-03-21 15:20:00 +0100

    throw domain exception instead IllegalArgumentException

--- src/main/java/freelance/domain/models/entity/Auditable.java
+++ src/main/java/freelance/domain/models/entity/Auditable.java
@@ -1,11 +1,13 @@
 package freelance.domain.models.entity;
 
+import freelance.domain.exception.DomainException;
+
 import java.time.LocalDateTime;
 
 public abstract class Auditable {
     Auditable(LocalDateTime createdDate,LocalDateTime updatedDate){
         if(createdDate.isAfter(updatedDate))
-            throw new IllegalArgumentException("created date can't be before updatedDate");
+            throw new DomainException("created date can't be before updatedDate");
         this.createdDate=createdDate; this.updatedDate=updatedDate;
     }
     Auditable(){
