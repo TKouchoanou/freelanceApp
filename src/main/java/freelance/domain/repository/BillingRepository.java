2f3ef270db7f0283d805147695ffb0aaf12eb412
Theophane Kouchoanou <ptitkouche@yahoo.fr>
2023-03-25 00:47:11 +0100

    add find by company and rib id method

--- src/main/java/freelance/domain/repository/BillingRepository.java
+++ src/main/java/freelance/domain/repository/BillingRepository.java
@@ -2,6 +2,8 @@ package freelance.domain.repository;
 
 import freelance.domain.models.entity.Billing;
 import freelance.domain.models.objetValue.BillingId;
+import freelance.domain.models.objetValue.CompanyId;
+import freelance.domain.models.objetValue.RibId;
 
 
 import java.util.Set;
@@ -9,4 +11,6 @@ import java.util.stream.Stream;
 
 public interface BillingRepository extends CrudRepository<Billing, BillingId>{
     Stream<Billing> findAll(Set<BillingId> billingIds);
+    Stream<Billing> findByRibId(RibId ribId);
+    Stream<Billing> findByCompanyId(CompanyId companyId);
 }
--- src/main/java/freelance/domain/repository/FreelanceRepository.java
+++ src/main/java/freelance/domain/repository/FreelanceRepository.java
@@ -2,7 +2,9 @@ package freelance.domain.repository;
 
 import freelance.domain.models.entity.Company;
 import freelance.domain.models.entity.Freelance;
+import freelance.domain.models.objetValue.CompanyId;
 import freelance.domain.models.objetValue.FreelanceId;
+import freelance.domain.models.objetValue.RibId;
 import freelance.domain.models.objetValue.UserId;
 
 import java.util.Optional;
@@ -11,5 +13,6 @@ import java.util.stream.Stream;
 public interface FreelanceRepository extends CrudRepository<Freelance, FreelanceId>{
 
     Optional<Freelance> findByUserId(UserId userId) ;
-    Stream<Freelance> findByCompany(Company company) ;
+    Stream<Freelance> findByCompany(CompanyId companyId) ;
+    Stream<Freelance> findByRibId(RibId ribId);
 }
