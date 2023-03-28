0e77f61a5e84c9513a54ecb222030e3f6dfbe3c8
Theophane Kouchoanou <ptitkouche@yahoo.fr>
2023-03-28 23:09:13 +0200

    create equal and hash method for email objet value

--- src/main/java/freelance/domain/models/objetValue/Email.java
+++ src/main/java/freelance/domain/models/objetValue/Email.java
@@ -3,10 +3,24 @@ package freelance.domain.models.objetValue;
 import freelance.domain.exception.DomainException;
 import org.apache.commons.validator.routines.EmailValidator;
 
+import java.util.Objects;
+
 public record Email (String value){
     public Email {
         if (!EmailValidator.getInstance().isValid(value)) {
             throw new DomainException("Invalid email");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Email email)) return false;
+        return Objects.equals(value, email.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
