c8723048f40e826871930b49be0736d933940f4c
Theophane Kouchoanou <ptitkouche@yahoo.fr>
2023-03-30 23:39:48 +0200

    store billing file

--- src/main/java/freelance/application/command/handler/billing/CreateBillingCommandHandler.java
+++ src/main/java/freelance/application/command/handler/billing/CreateBillingCommandHandler.java
@@ -1,22 +1,22 @@
 package freelance.application.command.handler.billing;
 
+import freelance.application.command.Command;
+import freelance.application.command.CommandHandler;
+import freelance.application.command.command.billing.CreateBillingCommand;
+import freelance.application.command.utils.AuthProvider;
 import freelance.domain.core.entity.Billing;
 import freelance.domain.core.entity.Company;
 import freelance.domain.core.entity.Rib;
 import freelance.domain.core.entity.User;
 import freelance.domain.core.objetValue.*;
+import freelance.domain.output.files.FileStorageService;
 import freelance.domain.output.repository.BillingRepository;
 import freelance.domain.output.repository.CompanyRepository;
 import freelance.domain.output.repository.RibRepository;
 import freelance.domain.output.repository.UserRepository;
-import freelance.application.command.Command;
-import freelance.application.command.CommandHandler;
-import freelance.application.command.command.billing.CreateBillingCommand;
-import freelance.application.command.utils.AuthProvider;
 import org.javamoney.moneta.Money;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 @Service
 public class CreateBillingCommandHandler implements CommandHandler {
@@ -25,9 +25,11 @@ public class CreateBillingCommandHandler implements CommandHandler {
     CompanyRepository companyRepository;
     RibRepository ribRepository;
     AuthProvider authProvider;
+    FileStorageService fileStorageService;
     CreateBillingCommandHandler(  BillingRepository billingRepository,
     UserRepository userRepository,
     CompanyRepository companyRepository,
+    FileStorageService fileStorageService,
     RibRepository ribRepository,
     AuthProvider authProvider){
         this.billingRepository=billingRepository;
@@ -35,6 +37,7 @@ public class CreateBillingCommandHandler implements CommandHandler {
         this.userRepository=userRepository;
         this.ribRepository=ribRepository;
         this.authProvider=authProvider;
+        this.fileStorageService=fileStorageService;
     }
     @Override
     public void handle(Command command, HandlingContext handlingContext) {
@@ -49,8 +52,13 @@ public class CreateBillingCommandHandler implements CommandHandler {
         Period period= new Period(cmd.getStartedDate(),cmd.getEndedDate());
         Money htAmount= Money.of(cmd.getAmountHT(), "EUR");
         Money ttcAmount= Money.of(cmd.getAmountTTC(),"EUR");
-        BillingFile billingFile= new BillingFile(cmd.getFile(), Optional.ofNullable(cmd.getFileName()).orElse("billing_"+user.getFirstName()+"_"+user.getId().id()+ LocalDateTime.now()));
+        //store billing file
+        File file=  fileStorageService.store(new File(null, cmd.getFile(),"billing"));
+        BillingFile billingFile= new BillingFile(file.id(),file.context(),cmd.getFileName());
+        
+        //save the billing
         Billing billing = billingRepository.save(new Billing(user,rib,company,period,ttcAmount,htAmount,billingFile));
+      //valorise command id and status
         cmd.setBillingId(billing.getId().id());
         cmd.setValidationStatus(billing.getValidationStatus().name());
         cmd.setPaymentStatus(billing.getPaymentStatus().name());
